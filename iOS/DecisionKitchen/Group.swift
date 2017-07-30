679d375bd3ccbcb5a11764021bc6be4590c73cc5
Andy Liang <meteochu@gmail.com>
2017-07-29 20:28:14 -0700

    Added Firebase model, and show Groups list

---
+++ iOS/DecisionKitchen/Group.swift
@@ -0,0 +1,27 @@
+//
+//  Group.swift
+//  DecisionKitchen
+//
+//  Created by Andy Liang on 2017-07-29.
+//  Copyright © 2017 Andy Liang. All rights reserved.
+//
+
+import UIKit
+
+typealias UserID = String
+
+struct Group: Codable {
+    
+    var name: String
+    
+    var password: String
+    
+    var members: [UserID]
+    
+    var restaurants: [String: Restaurant]
+    
+    var games: [Game]
+    
+    var id: String
+    
+}
---
+++ iOS/DecisionKitchen/GroupDetailCell.swift
@@ -0,0 +1,29 @@
+//
+//  GroupDetailCell.swift
+//  DecisionKitchen
+//
+//  Created by Andy Liang on 2017-07-29.
+//  Copyright © 2017 Andy Liang. All rights reserved.
+//
+
+import UIKit
+
+class GroupDetailCell: UITableViewCell {
+    
+    var group: Group! {
+        didSet {
+            self.textLabel?.text = group.name
+            self.detailTextLabel?.text = group.password
+        }
+    }
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        self.accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
