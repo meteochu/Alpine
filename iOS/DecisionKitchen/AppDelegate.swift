be81ad7b5e2875d61ab46ecd32b92d9d663b64f5
Andy Liang <meteochu@gmail.com>
2017-07-29 17:36:02 -0700

    Added Firebase + Facebook Auth

--- iOS/DecisionKitchen/AppDelegate.swift
+++ iOS/DecisionKitchen/AppDelegate.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
-import FacebookLogin
+import Firebase
+import FacebookCore
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Successfully Registered Remote Notifications...")
+            } else {
+                print(error ?? "")
+            }
+        }
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // did receive notification
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // will present notification
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        // deep linking...
+        print(#function, url)
+        let handled = SDKApplicationDelegate.shared.application(app, open: url, options: options)
+        return handled
     }
     
 }
---
+++ iOS/DecisionKitchen/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  DecisionKitchen
+//
+//  Created by Andy Liang on 2017-07-29.
+//  Copyright © 2017 Andy Liang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
