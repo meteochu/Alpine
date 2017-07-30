18874dfc4d34131e18c94de32890ce8bfca29991
Andy Liang <meteochu@gmail.com>
2017-07-29 20:26:37 -0700

    Added Firebase model, and show Groups list

---
+++ iOS/DecisionKitchen/Order.swift
@@ -0,0 +1,17 @@
+//
+//  Order.swift
+//  DecisionKitchen
+//
+//  Created by Andy Liang on 2017-07-29.
+//  Copyright © 2017 Andy Liang. All rights reserved.
+//
+
+import Foundation
+
+struct Order: Codable {
+    
+    var id: String
+    
+    var service: String
+    
+}
