3a99b85e9f55ac97bb13302bf03b02afd8f82f7e
Kevin Pei <kevin-pei@hotmail.com>
2017-07-30 07:31:18 -0700

    Alex

--- Android/app/src/main/java/com/decisionkitchen/decisionkitchen/GameActivity.kt
+++ Android/app/src/main/java/com/decisionkitchen/decisionkitchen/GameActivity.kt
@@ -4,34 +4,70 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import java.util.*
 
+data class Question (val title: String, val options: Array<String>);
+
 class GameActivity : Activity() {
 
     private var group : Group? = null
     private var groupRef: DatabaseReference? = null
-    private var question: Int = 0
+    private var question: Int = -1
     private var gameId: Int? = null
     private var user: FirebaseUser? = null
     private var qOrder: ArrayList<Int> = ArrayList<Int>()
+    private var questions: ArrayList<Question> = arrayListOf(
+        Question("How much do you want to spend?", arrayOf("$", "$$", "$$$", "$$$$")),
+        Question("What kind of food do you want?", arrayOf("Breakfast & Brunch", "Chinese", "Diners", "Fast Food", "Hot Pot", "Italian", "Japanese", "Korean", "Mongolian", "Pizza", "Steakhouses", "Sushi Bars", "American (Traditional)", "Vegetarian")),
+        Question("Delivery of dine-in?", arrayOf("Delivery", "Dine-in"))
+    )
+    private var responses: ArrayList<ArrayList<Int>> = ArrayList<ArrayList<Int>>();
 
     fun render() {
-        if (group != null && groupRef != null && qOrder != null && user != null && gameId != null) {
+        if (group != null && groupRef != null && qOrder != null && question != -1 && user != null && gameId != null) {
+            val q = questions[question]
+            (findViewById(R.id.question) as TextView).text = q.title
+            val optionsLayout: LinearLayout = (findViewById(R.id.options) as LinearLayout)
+            optionsLayout.removeAllViews()
+
+            for ((index, option) in q.options.withIndex()) {
+                var button = Button(optionsLayout.context)
+                button.text = (if (responses[question][index] == 0) "Select " else "Deselect ") + option
+                button.setOnClickListener {
+                    responses[question][index] = 1 - responses[question][index]
+                    render()
+                }
+                optionsLayout.addView(button)
+            }
 
         }
     }
 
     fun nextQuestion() {
         val game = group!!.games!![gameId!!]
-        Log.w("text", qOrder.toString())
+        question++
+        if (question < questions.size) render()
+        else {
+            groupRef!!.child("games").child(gameId.toString()).child("responses").child(user!!.uid).setValue(responses)
+        }
+    }
+
+    public fun nextQuestion(view: View) {
+        nextQuestion()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        responses = ArrayList<ArrayList<Int>>(questions.size);
 
         val database = FirebaseDatabase.getInstance()
         user = FirebaseAuth.getInstance().currentUser;
@@ -46,16 +82,16 @@ class GameActivity : Activity() {
                 if (gameId == null) {
                     for (index in 0 .. (group!!.games!!.size - 1)) {
                         val game = group!!.games!![index]
-                        Log.w("test", game.toString())
                         if (game.meta!!.end == null && (game.responses == null || !game.responses.containsKey(user!!.uid))) {
                             gameId = index
-                            groupRef!!.child("games").child(index.toString()).child("responses").child(user!!.uid)
-                                    .setValue(IntArray(game.categories!!.size).toList())
-
-                            for (i in 0 .. (game.categories.size - 1)) {
+                            for (i in 0 .. (questions.size - 1)) {
                                 qOrder.add(i)
+                                val tmp = ArrayList<Int>(questions[i].options.size)
+                                for (j in 0 .. questions[i].options.size - 1) {
+                                    tmp.add(0)
+                                }
+                                responses.add(tmp);
                             }
-
                             Collections.shuffle(qOrder);
                             nextQuestion()
                         } else {
