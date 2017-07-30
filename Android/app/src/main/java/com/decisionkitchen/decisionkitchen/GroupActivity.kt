4c96a9bb3eb617032cd440c9c9f211d0a9be96c5
Kevin Pei <kevin-pei@hotmail.com>
2017-07-29 21:51:15 -0700

    Fix group layout

--- Android/app/src/main/java/com/decisionkitchen/decisionkitchen/GroupActivity.kt
+++ Android/app/src/main/java/com/decisionkitchen/decisionkitchen/GroupActivity.kt
@@ -1,7 +1,9 @@
 package com.decisionkitchen.decisionkitchen
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.util.Log
@@ -10,8 +12,17 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
 
 class GroupActivity : Activity() {
+
+    private fun getContext(): Context {
+        return this
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +37,61 @@ class GroupActivity : Activity() {
 
         val groupListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get Post object and use the values to update the UI
+
                 val group : Group = dataSnapshot.getValue<Group>(Group::class.java)!!
                 toolbar.title = group.name
+
+                findViewById(R.id.loader).visibility = View.INVISIBLE
+                findViewById(R.id.content).visibility = View.VISIBLE
+
+
+                val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                val layout = LinearLayout(getContext())
+                val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                layout.layoutParams = layoutParams
+                for (member_id in group.members!!) {
+                    val groupLayout = LinearLayout(layout.context)
+                    groupLayout.orientation = LinearLayout.VERTICAL
+                    val groupLayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                    groupLayout.layoutParams = groupLayoutParams
+                    val title = TextView(layout.context)
+
+                    val group = data.getValue(Group::class.java)
+                    title.text = group!!.name
+                    title.textSize = 20f
+                    title.width
+                    title.setTextColor(Color.BLACK)
+                    title.setPadding(0, 5, 0, 15)
+                    groupLayout.addView(title)
+
+                    val password = TextView(layout.context)
+                    password.text = group.password
+                    password.textSize = 13f
+                    password.setPadding(0, 5, 0, 15)
+                    password.setTextColor(Color.GRAY)
+                    groupLayout.addView(password)
+
+                    val divider = LinearLayout(layout.context)
+                    val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 2)
+                    divider.setBackgroundColor(Color.BLACK)
+                    divider.layoutParams = params
+                    groupLayout.addView(divider)
+
+                    layout.addView(groupLayout)
+                    groupLayout.setOnClickListener(object : View.OnClickListener {
+                        override fun onClick(v: View?) {
+                            goTest(data.key)
+                        }
+                    })
+                }
+                scrollView.removeAllViews()
+                scrollView.addView(layout)
+
+
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
                 Log.w("dk", "loadPost:onCancelled", databaseError.toException())
                 // ...
             }
