16f93c4af5e837f8defb7f247d3245a239226976
Kevin Pei <kevin-pei@hotmail.com>
2017-07-29 20:02:35 -0700

    Add typealiases

--- Android/app/src/main/java/com/decisionkitchen/decisionkitchen/Group.kt
+++ Android/app/src/main/java/com/decisionkitchen/decisionkitchen/Group.kt
@@ -4,28 +4,32 @@ package com.decisionkitchen.decisionkitchen
  * Created by kevin on 2017-07-29.
  */
 
+typealias RestaurantID = String;
+typealias GroupID = String;
+typealias UserID = String;
+
 data public class Category(val name: String? = null, val type: String? = null, val value: Any? = null)
 
 data public class RestaurantOrder(val id: String? = null, val service: String? = null)
 
-data public class Restaurant(val address: String? = null, val name: String? = null, val id: String? = null, val order: RestaurantOrder? = null)
+data public class Restaurant(val address: String? = null, val name: String? = null, val id: RestaurantID? = null, val order: RestaurantOrder? = null)
 
 data public class GameMeta(val end: String? = null, val start: String? = null)
 
-data public class GameResult(val restaurant_id: String? = null)
+data public class GameResult(val restaurant_id: RestaurantID? = null)
 
 data public class Game(
     val categories: ArrayList<Category>? = null,
     val meta: GameMeta? = null,
-    val rating: HashMap<String, Integer>? = null,
-    val responses: HashMap<String, ArrayList<Integer>>? = null,
+    val rating: HashMap<UserID, Integer>? = null,
+    val responses: HashMap<UserID, ArrayList<Integer>>? = null,
     val result: GameResult? = null)
 
 data public class Group(
     val password: String? = null,
-    val members: ArrayList<String>? = null,
+    val members: ArrayList<UserID>? = null,
     val name: String? = null,
-    val restaurants: HashMap<String, Restaurant>? = null,
-    val id: String? = null,
+    val restaurants: HashMap<RestaurantID, Restaurant>? = null,
+    val id: GroupID? = null,
     val games: ArrayList<Game>? = null
 )
